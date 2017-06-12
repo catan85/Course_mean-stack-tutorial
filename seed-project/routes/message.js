6497cece6e99fe24400b79d956cd419a8e7a6b38
catan85 <catan85@gmail.com>
2017-06-12 07:33:09 +0200

    fine

--- seed-project/routes/message.js
+++ seed-project/routes/message.js
@@ -1,4 +1,4 @@
-var express = require('express');
+﻿var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
 var Message = require('../models/message');
